7b2ab3c49873768f472382673dbeeb53e0e20724
Wahyu Ramadan <wramadan1203@gmail.com>
2025-05-21 13:17:47 +0700

    style(EffectiveSocialMediaManagement): change breakpoint to show slide

--- src/components/slides/EffectiveSocialMediaManagementSlide.tsx
+++ src/components/slides/EffectiveSocialMediaManagementSlide.tsx
@@ -24,7 +24,7 @@ export default function EffectiveSocialMediaManagementSlide({
     prevArrow: <PrevArrowSlider />,
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
