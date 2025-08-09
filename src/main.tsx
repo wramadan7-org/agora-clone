8f800d25907c53d57d56940b740ac187137bbad2
Wahyu Ramadan <wramadan1203@gmail.com>
2025-08-09 16:23:48 +0700

    chore: add speedinsigts to vercel detection

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { RouterProvider } from "react-router";
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import routers from "./routes/route";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
+    <SpeedInsights />
     <StrictMode>
       <RouterProvider router={routers} />
     </StrictMode>
